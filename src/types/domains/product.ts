da4c0667573046e73d744140b1c4946a154c93f9
musharraf48 <musharrafansari7987@gmail.com>
2025-09-22 16:53:03 +0530

    feat: Integrate shipping method selection in product forms and update related data handling

--- src/types/domains/product.ts
+++ src/types/domains/product.ts
@@ -4,6 +4,7 @@ import { Attribute } from "./attribute";
 export interface Product {
     productId: number;
     categoryId: number;
+    shippingMethodId?: number;
     title: string;
     code: string;
     description: string;
@@ -48,6 +49,7 @@ export interface ProductDetails {
     title: string;
     code: string;
     categoryId: number;
+    shippingMethodId?: number;
     variants: (Omit<ProductVariant, 'variationOptions'> & {variantProperties : {[variationId: number] : {variationOptionId: number , name: string}}})[];
     description: string;
     attributes: ProductAttribute[];
@@ -58,6 +60,7 @@ export interface ProductPatchPayload {
     title: string;
     starred: boolean;
     categoryId: number;
+    shippingMethodId?: number;
 }
 
 export interface ProductPayload {
@@ -66,6 +69,7 @@ export interface ProductPayload {
     description: string;
     starred: boolean;
     categoryId: number;
+    shippingMethodId?: number;
     images: {
         productImageId?: number;
         imageUrl: string;
@@ -89,6 +93,7 @@ export interface ProductPreview {
     productId: number;
     productVariantId: number;
     categoryId: number;
+    shippingMethodId?: number;
     dateAdded: Date;
     quantityInStock: number;
     imageUrl: string;
